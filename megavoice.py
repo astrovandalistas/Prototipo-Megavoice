5ae35e0a24dac897508b22038352fcae6e0f58fd
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-11 02:45:51 -0700

    initial commit of megavoice prototype code. just to test prototypeInterface

---
+++ megavoice.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("../LocalNet")
+from interfaces import PrototypeInterface, runPrototype
+
+class Megavoice(PrototypeInterface):
+    """ Megavoice prototype class
+        all prototypes must define setup() and loop() functions
+        self.messageQ will have all messages coming in from LocalNet """
+    def setup(self):
+        ## pick what to subscribe to
+        pass
+    def loop(self):
+        ## check state
+        ## if (not self.messageQ.empty())
+        pass
+
+if __name__=="__main__":
+    ## TODO: get ip and ports from command line
+    mM = Megavoice(8989,"127.0.0.1",8888)
+    runPrototype(mM)
