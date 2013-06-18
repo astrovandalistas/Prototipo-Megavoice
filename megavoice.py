cd6b23b21597ae18d931906305dd2daaff400ec7
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-18 00:56:29 -0700

    adds queue processing code

--- megavoice.py
+++ megavoice.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 sys.path.append("../LocalNet")
 from interfaces import PrototypeInterface, runPrototype
 
@@ -12,10 +12,22 @@ class Megavoice(PrototypeInterface):
         ## or....
         for k in self.allReceivers.keys():
             self.subscribeTo(k)
+        ## some variables
+        self.queueDelay = 30
+        self.lastQueueCheck = time.time()
     def loop(self):
         ## check state
-        ## if (not self.messageQ.empty())
-        pass
+        if ((not self.messageQ.empty()) and
+            (time.time() - self.lastQueueCheck > self.queueDelay)):
+            (locale,type,txt) = self.messageQ.get()
+            print "got: "+txt
+            self.lastQueueCheck = time.time()
+            if(self.messageQ.qsize() > 50):
+                self.queueDelay = 10
+            elif(self.messageQ.qsize() > 10):
+                self.queueDelay = 20
+            else:
+                self.queueDelay = 30
 
 if __name__=="__main__":
     ## TODO: get ip and ports from command line
