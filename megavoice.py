e8525b7c4a62fb5cd5ca5261cbd87398ff06cc72
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-18 03:01:32 -0700

    uses festival and subprocesses to say messages

--- megavoice.py
+++ megavoice.py
@@ -1,6 +1,13 @@
-import sys, time
+# -*- coding: utf-8 -*-
+
+import sys, time, subprocess
 sys.path.append("../LocalNet")
 from interfaces import PrototypeInterface, runPrototype
+
+FESTIVAL_ES = "voice_cstr_upc_upm_spanish_hts"
+FESTIVAL_EN = "voice_cstr_upc_upm_spanish_hts"
+FESTIVALBIN = "/Users/thiago/Dev/festival-2.1/SpanishHTSVoice-ver0.8/build/festival/bin/festival"
+FESTIVALCMD = "echo \"(LANG) (SayText \\\"XXXXX\\\")\" | "
 
 class Megavoice(PrototypeInterface):
     """ Megavoice prototype class
@@ -20,7 +27,16 @@ class Megavoice(PrototypeInterface):
         if ((not self.messageQ.empty()) and
             (time.time() - self.lastQueueCheck > self.queueDelay)):
             (locale,type,txt) = self.messageQ.get()
-            print "got: "+txt
+            txt = txt.replace("#","")
+            ## TODO: detect language!
+            ## TODO: sanitize text message
+            ##       #tag -> something else
+            ##       á -> aa, etc
+            ## then remove any nonAscii characters
+            txt = self.removeNonAscii(txt.encode('utf-8'))
+            toSay = (FESTIVALCMD+FESTIVALBIN).replace("LANG",FESTIVAL_ES)
+            toSay = toSay.replace("XXXXX",txt)
+            subprocess.call(toSay, shell=True)
             self.lastQueueCheck = time.time()
             if(self.messageQ.qsize() > 50):
                 self.queueDelay = 10
