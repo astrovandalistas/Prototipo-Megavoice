be8bfa1cc6491778defb8df7b9ee40819960a5ea
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-07-12 01:39:18 -0700

    adds language detection

--- megavoice.py
+++ megavoice.py
@@ -3,9 +3,10 @@
 import sys, time, subprocess
 sys.path.append("../LocalNet")
 from interfaces import PrototypeInterface, runPrototype
+import langid
 
 FESTIVAL_ES = "voice_cstr_upc_upm_spanish_hts"
-FESTIVAL_EN = "voice_cstr_upc_upm_spanish_hts"
+FESTIVAL_EN = "voice_kal_diphone"
 FESTIVALBIN = "./festival"
 FESTIVALCMD = "echo \"(LANG) (SayText \\\"XXXXX\\\")\" | "
 
@@ -28,15 +29,21 @@ class Megavoice(PrototypeInterface):
         self.lastQueueCheck = time.time()
         ## turn up the volume
         subprocess.call("amixer set PCM -- -0", shell=True)
+
+        ## for language identification
+        langid.set_languages(['en','es'])
     def loop(self):
         ## check state
         if ((not self.messageQ.empty()) and
             (time.time() - self.lastQueueCheck > self.queueDelay)):
             (locale,type,txt) = self.messageQ.get()
-            ## TODO: detect language!
+
+            ## detect language!
+            mLanguage = FESTIVAL_ES if(langid.classify(txt)[0] == 'es') else FESTIVAL_EN
+
             ## then remove accents and nonAscii characters
             txt = self.removeNonAscii(self.removeAccents(txt.encode('utf-8')))
-            toSay = (FESTIVALCMD+FESTIVALBIN).replace("LANG",FESTIVAL_ES)
+            toSay = (FESTIVALCMD+FESTIVALBIN).replace("LANG",mLanguage)
             toSay = toSay.replace("XXXXX",txt)
             subprocess.call(toSay, shell=True)
             self.lastQueueCheck = time.time()
