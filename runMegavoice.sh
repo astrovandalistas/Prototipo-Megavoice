a7d40780b0c1f59b2b4f903cef32dd82b75319d7
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-07-17 22:34:48 -0700

    adds equal-sign and ip/names to run script

--- runMegavoice.sh
+++ runMegavoice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start Megavoice
-python megavoice.py --inport 8989 --localnetport 8900 --localnet 10.0.0.3 --inip 10.0.0.3
+python megavoice.py --inport=8989 --localnetport=8900 --localnet=aeffect07.local --inip=aeffect02.local
